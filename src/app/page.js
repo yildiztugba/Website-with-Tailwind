2017793529849551baa72dd9ee10e1ce9b9454ff
yildiztugba <okra.yildiz@gmail.com>
2023-03-28 09:56:36 +0200

    css for h1,a,bsfillmoonstartsfill

--- src/app/page.js
+++ src/app/page.js
@@ -12,13 +12,13 @@ export default function Home() {
       
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between">
-          <h1>developedbyyildiz</h1>
+          <h1 className="text-xl">developedbyyildiz</h1>
           <ul>
             <li>
-              <BsFillMoonStarsFill />
+              <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
             </li>
             <li>
-              <a href="#">Resume</a>
+              <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
             </li>
           </ul>
         </nav>
